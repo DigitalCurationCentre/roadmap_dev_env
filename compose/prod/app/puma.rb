1b90e2b4238bd7ee6e502832ba5cb55d5c775215
Colin Gormley <colin.gormley@ed.ac.uk>
2018-09-14 11:43:48 +0100

    Initial attempt at getting branded dmponline working with Apache and Puma in Docker
    - Note that this is still very much a work in progress.
    - The project has been reorganised.  We now have a compose directory which contains settings and docker files for different components.  For now, only prod has been updated.  It contains separate docker files for Puma and Apache.  Most of the Apache config has been lifted from the new AT server. 
    - Added docker-compose dev and prod files.  Ignore dev for now as it hasn’t been updated. This might change - docker compose supports inheritance so we might be best to have a base docker compose file and have prod and dev override it as needed.  This would should help reduce config duplication.
    - docker-compose-prod.yml is still a work in progress.  Currently it sets up a Puma server for dmponline and configures Apache with 4 virtual hosts for each of the dmp services.  Still to create corresponding Puma servers for the other services. It may be that running an Apache container with 4 virtual hosts isn’t what we want to do in terms of bringing individual instances up and down, but it will do initially. Note that the Apache docker file sets each service ports and names as environment variables and these are then interpolated into various configuration files.  The dmponline virtual host config has been set to serve static resources from Apache - this probably needs checked, but seems to be working.   Note that we are using a shared volume so that Apache can access the static dmponline resources that have been added as part of the app Dockerfile.  I had issues with this on the Mac - I had to removed ‘Users’ from the File Sharing list in the Docker app to enable the Apache container to access the volume. The app Dockerfile configures Puma, copies in the dmponline branding (I’ve replicated the steps from the deploy shell scripts we currently use) and runs the various npm and bundle commands. 
    - In order to run, you will have to checkout the roadmap and and roadmap-branding repos into the project directory.  This process should probably be updated to use git submodules. Note that in the roadmap repo, ensure that puma is present in the Gemfile. In the roadmap repo, for now you will have to update the dmponline database and secrets yml files similar to the config examples in this project.  In future, these changes would ideally be committed to the branding repo.
    - To run, you can use the following (if you’re not making changes, you can omit the no-cache directive):
    docker-compose -f docker-compose.prod.yml build --no-cache app web; docker-compose -f docker-compose.prod.yml up

---
+++ compose/prod/app/puma.rb
@@ -0,0 +1,34 @@
+# Change to match your CPU core count
+workers 2
+
+# Min and Max threads per worker
+threads 1, 6
+
+app_dir = "$RAILS_ROOT"
+shared_dir = "#{app_dir}/puma/$SERVICE_NAME"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+# TODO Need to set up production environment
+# rails_env = "development"
+
+# Set up socket location
+# This form doesn't work
+# bind "tcp://localhost:3000/"
+bind 'tcp://0.0.0.0:$PUMA_PORT'
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
