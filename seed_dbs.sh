7fb900c434b014fd2c295c04a75b67bd8e1ce2b5
Colin Gormley <colin.gormley@ed.ac.uk>
2018-10-11 14:42:14 +0100

    Added seed_dbs.sh script as a convenience to seed container abs
    - Runs the db:setup rake command for each of the apps defined in the compose file, e.g docker-compose -f docker-compose.prod.yml run --rm dmptuuli_test_app rake db:setup

---
+++ seed_dbs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Convenience to seed all dbs
+
+function seedDbs {
+    containers=( "dmponline_app" "dmpmelbourne_app" "dmptuuli_app" "dmponline_dev_app" "dmptuuli_test_app" )
+    
+    for i in "${containers[@]}"
+    do
+        command="docker-compose -f docker-compose.prod.yml run --rm $i rake db:setup"
+        echo Running $command
+        eval $command
+    done
+}
+
+seedDbs
+exit 0
